53c3e6b302c5e6f6f95b49568cd39a8237c6b781
deej-0101 <diane.joy.d.n@gmail.com>
2024-01-09 02:50:39 +0800

    a

--- magic-master-app/magic-project/magic_app/views.py
+++ magic-master-app/magic-project/magic_app/views.py
@@ -21,6 +21,9 @@ class MagicalCreatureListView(ListView):
     context_object_name = 'creatures'
     template_name = 'creature_list.html'
     paginate_by = 5  # Adjust the number of items per page as needed
+    
+    def get_queryset(self):
+        return MagicalCreature.objects.order_by('name', 'species', 'magical_ability')
 
 # DRIVER
 class WizardListView(ListView):
@@ -28,6 +31,9 @@ class WizardListView(ListView):
     context_object_name = 'wizards'
     template_name = 'wizard_list.html'
     paginate_by = 3  # Adjust the number of items per page as needed
+     
+    def get_queryset(self):
+        return Wizard.objects.order_by('image', 'name', 'email', 'magical_abilities', 'birth_date', 'magical_wand')
 
 
 class EnchantedPlaceListView(ListView):
@@ -35,16 +41,25 @@ class EnchantedPlaceListView(ListView):
     context_object_name = 'places'
     template_name = 'place_list.html'
     paginate_by = 5  # Adjust the number of items per page as needed
+    
+    def get_queryset(self):
+        return EnchantedPlace.objects.order_by('name', 'location', 'enchantment_level')
 
 class MagicalEncounterListView(ListView):
     model = MagicalEncounter
     context_object_name = 'encounter'
     template_name = 'encounter_list.html'
     paginate_by = 5  # Adjust the number of items per page as needed
+    
+    def get_queryset(self):
+        return MagicalEncounter.objects.order_by('event', 'wizard', 'creature', 'outcome')
 
 class MagicalEventListView(ListView):
     model = MagicalEvent
     context_object_name = 'event'
     template_name = 'event_list.html'
     paginate_by = 5  # Adjust the number of items per page as needed
+    
+    def get_queryset(self):
+        return MagicalEvent.objects.order_by('title', 'date', 'place')
 
